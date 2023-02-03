4ca47a7ef9c7af78a1513f003ce174f836a422c5
quThinh <quangthinh0101@gmail.com>
2023-02-03 16:31:03 +0700

    update route

--- routes/articles.js
+++ routes/articles.js
@@ -7,13 +7,11 @@ const ArticleController = require('../controllers/articles')
 
 router.get('/articles', ArticleController.getAllArticles)
 router.post('/articles',authByToken ,ArticleController.createArticle)
-router.put('/articles/:id', authByToken,ArticleController.updateArticle)
+router.patch('/articles/:id', authByToken,ArticleController.updateArticle)
 router.get('/articles/:id', ArticleController.getDetailArticleById)
 router.post('/articles/report',authByToken, ArticleController.reportArticle)
 
-router.get('articles/user/:id', ArticleController.getAllArticles)
-router.post('/articles/report/:id', ArticleController.getAllArticles)
-router.get('/articles/report/:id', ArticleController.getAllArticles)
-router.post('/articles/censor/:id', ArticleController.getAllArticles)
+router.get('/articles/user/:id', ArticleController.getAllArticles)
+// router.post('/articles/censor/:id', ArticleController.getAllArticles)
 
 module.exports = router
